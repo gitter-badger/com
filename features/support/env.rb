d4ed476ee6139497c2fc1b56c3f2de886c6bd498
Geoff Massanek <gmassanek@gmail.com>
2015-06-11 01:40:41 -0500

    Add the bind polyfill to just poltergeist

--- features/support/env.rb
+++ features/support/env.rb
@@ -12,7 +12,7 @@ Capybara.app = Builders::FULL_STACK
 headless = (ENV["HEADLESS"] == "true")
 if headless
   Capybara.register_driver(:poltergeist) do |app|
-    Capybara::Poltergeist::Driver.new(app, { debug: false })
+    Capybara::Poltergeist::Driver.new(app, { debug: false, extensions: ["vendor/poltergeist/bind.js"] })
   end
 else
   Capybara.register_driver(:browser) do |app|
